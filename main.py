addfaee7a0f4b7c89bb8b5f1ddf4691b3e600569
Stephenpaps <stephenjosephdurai@gmail.com>
2022-07-03 10:04:11 +0530

    Automated till Login page and waiting for Datepicker and Dropdown automation.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import helium
 from helium import *
 
-
-start_chrome(url='www.legacynow.com', maximize=False)
+"""
+#Registration Page
+start_chrome(url='https://dlb.estate-registry.ca', maximize=True)
 click(Button('Get Started'))
 write('Velan', into='First Name')
 write('Balasubramaniam', into='Last Name')
@@ -10,4 +11,23 @@ write('velbal@mailinator.com', into='Email Address')
 write('6879967290', into='Your Phone Number')
 write('123456', into='Create Password')
 click(Button('Register'))
+
+# Mail Verification
 go_to('mailinator.com')
+write('velbal')
+click('go')
+click('LegacyNOW')
+scroll_down(250)
+click(Link('Verify Email Address'))"""
+
+start_chrome(url='dlb.estate-registry.ca/login', maximize=True)
+write('velbal@mailinator.com', into='Email Address')
+write('123456', into='Password')
+click(Button('Login'))
+click(Button('Get Started'))
+
+
+
+
+
+
